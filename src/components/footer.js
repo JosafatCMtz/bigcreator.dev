f66a8091129c4b30c69fd8219485a5de5742e3ed
Josafat <josamtz@outlook.com>
2022-08-01 15:14:44 -0500

    🐙🔥 Footer Section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,28 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-wrap items-center">
-      <p className="text-smoky-black">
-        Made with ❤️ in MX © {new Date().getFullYear()} &middot; Bigcreator
-      </p>
+    <footer className="bg-smoky-black text-cultured justify-around">
+      <div className="grid lg:grid-cols-3 md:grid-cols-1">
+        <div className="flex items-center justify-center mb-6">
+          <StaticImage src="../images/icon_dark.png" alt="a bigcreator icon dark" className="w-44 h-44" />
+        </div>
+        <div className="flex items-center justify-center mb-6">
+          <a href="#!" className="mr-6">
+            <StaticImage src="../images/whatsapp-brands.png" alt="a bigcreator icon facebook" className="w-16 h-16" />
+          </a>
+          <a href="#!" className="mr-6">
+            <StaticImage src="../images/facebook-brands.png" alt="a bigcreator icon facebook" className="w-16 h-16" />
+          </a>
+          <a href="#!" className="mr-6">
+            <StaticImage src="../images/twitter-brands.png" alt="a bigcreator icon facebook" className="w-16 h-16" />
+          </a>
+        </div>
+        <div className="flex items-center justify-center mb-6">
+          <p>Made with ❤️ in MX © {new Date().getFullYear()} &middot; bigcreator.dev</p>
+        </div>
+      </div>
     </footer>
   );
 }
