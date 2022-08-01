a91f951b4dc968049e026558250fdf8f2683b3a4
Josafat <josamtz@outlook.com>
2022-08-01 17:02:46 -0500

    🐙🔥 About Us Section

---
+++ src/components/content.js
@@ -0,0 +1,18 @@
+const content = {
+    "about_us": {
+        "we_love": {
+            title: "We Love What We Do ♥️",
+            description: 'As a growing development center, we offer software solutions for all dimensions of business. Our specialized professional developers and designers help develop and design applications using the latest technologies for Web development, IOS and Android.'
+        },
+        mision: {
+            title: "Mision 🎯",
+            description: "At bigcreator.dev our mission is to specialize in business solutions. We use advanced technology to provide solutions to our clients.We regularly invest in exploring modern technologies that can benefit our customers in the long term."
+        },
+        vision: {
+            title: "Vision 🧐",
+            description: "We focus on innovative and customized solutions that offer an excellent user experience. Our vision is to provide optimal solutions, creating unique experiences for each type of client."
+        }
+    }
+}
+
+export default content
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import HeaderImage from "../components/headerImage";
+import AboutUs from "../components/aboutUs";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <div>
       <HeaderImage />
+      <AboutUs />
     </div>
   </Layout>
 );
